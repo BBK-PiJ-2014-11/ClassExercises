130af40224849389e5a98fab37c543bafc82bd52
Ehshan <eveerabangsa@gmail.com>
2014-11-13 18:15:02 +0000

    spies have been deployed

---
+++ day 7/src/spy.java
@@ -0,0 +1,46 @@
+/**
+ * Created by Ehshan on 10/11/2014.
+ */
+import java.util.Scanner;
+
+public class spy {
+    private static Scanner scanner = new Scanner(System.in);
+
+    private static int spyCount = 0;
+    private int id;
+    private String name;
+
+
+    public spy(int id){
+        System.out.println("Enter spy id)");
+        this.id = scanner.nextInt();
+        //this.id = id;
+        spyCount++;
+        System.out.println("spy ID: " + this.id);
+        System.out.println("spy Name: CONFIDENTIAL");
+        System.out.println("The number of spies deployed is: " + getSpyCount());
+    }
+
+
+    public static int getSpyCount(){
+        return spyCount;
+    }
+
+    public void die(){
+        spyCount--;
+        System.out.println("Spy " + this.id + " has failed in their mission");
+        System.out.println("The number of spies deployed is: " + getSpyCount());
+    }
+
+
+
+    public static void main(String[] args){
+        spy spy1= new spy(1);
+        spy spy2 = new spy(2);
+        spy spy3 = new spy(3);
+        spy spy4= new spy(4);
+        spy spy5= new spy(5);
+        spy1.die();
+    }
+
+}
