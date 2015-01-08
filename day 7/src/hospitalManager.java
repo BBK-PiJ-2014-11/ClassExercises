01d684eaea3a8b97c4f4934a65bbd9d00445d5e8
Ehshan <eveerabangsa@gmail.com>
2015-01-08 23:04:01 +0000

    files added

--- day 7/src/hospitalManager.java
+++ day 7/src/hospitalManager.java
@@ -13,21 +13,21 @@ public class hospitalManager {
         patientListStart = one;
         patient two = new patient("Dick", 72, "Flu");
         patientListStart.addPatient(two);
-        patient three = new patient("Harry", 43, "Aml");
+        patient three = new patient("Harry", 43, "Broken Leg");
         patientListStart.addPatient(three);
-        patient four = new patient("Sally", 20, "Sepsis");
+        patient four = new patient("Sally", 20, "Broken Heart");
         patientListStart.addPatient(four);
         patient five = new patient("Rita", 28, "Daibetic");
         patientListStart.addPatient(five);
         patient six = new patient("Mr Fisher", 40,"Unspecified");
         patientListStart.addPatient(six);
-        patient seven = new patient("Anon", 35, "Myeloma");
+        patient seven = new patient("Anon", 35, "Depression");
         patientListStart.addPatient(seven);
-        patient eight = new patient("Joe", 9, "Aml");
+        patient eight = new patient("Joe", 9, "Cold");
         patientListStart.addPatient(eight);
-        patient nine = new patient("Jane", 16, "Sepsis");
+        patient nine = new patient("Jane", 16, "Cancer");
         patientListStart.addPatient(nine);
-        patient ten = new patient("Mrs Etc", 60, "Daibetic");
+        patient ten = new patient("Mrs Etc", 60, "Unknown");
         patientListStart.addPatient(ten);
 
         System.out.println("Admissions");
