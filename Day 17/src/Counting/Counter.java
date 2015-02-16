f68e1830f0ea595b619919b74e9f3e13fef260a2
Ehshan <eveerabangsa@gmail.com>
2015-02-16 19:35:08 +0000

    added counter

---
+++ Day 17/src/Counting/Counter.java
@@ -0,0 +1,18 @@
+package Counting;
+
+/**
+ * Created by Ehshan on 09/02/2015.
+ */
+public class Counter {
+
+    private int n = 0;
+
+    public void increase() {
+        n++;
+    }
+
+    public int getCount() {
+        return n;
+    }
+
+}
